effc90500a92c82d5eb74910bff325cbe8aa1449
ivand <ivandvurechenskiy26@gmail.com>
2022-11-25 20:10:21 +0300

    Делаю карусель

--- lib/screen_3_reustarants/Widget/CathegoryCards.dart
+++ lib/screen_3_reustarants/Widget/CathegoryCards.dart
@@ -1,5 +1,30 @@
 import 'package:flutter/material.dart';
 
-class CathegoryCard extends StatelessWidget{
-  f
+class CathegoryCard extends StatelessWidget {
+  final String title;
+  final String image;
+  final double width;
+  const CathegoryCard(
+      {required this.title, required this.image, this.width = 100.0, Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Container(
+        width: width,
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(10.0),
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: buildTitle(context, title: title),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      );
 }
